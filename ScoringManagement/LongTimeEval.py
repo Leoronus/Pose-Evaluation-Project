4d25d6d8415be97d3f0c508520e343662311d860
LeonWigro <leon.wigro@alumni.fh-aachen.de>
2024-01-27 19:27:26 +0100

    Vorlage Langzeit-Funktion

---
+++ ScoringManagement/LongTimeEval.py
@@ -0,0 +1,5 @@
+
+def LongTimeEval(data) -> str:
+    return None
+'''Nimmt ein Array von Unix-Timestamps und MainBody-Objekten entgegen und gibt einen 
+String mit der für das GUI vorbereiteten Nachricht mit den Ergebnissen der Langzeitanalyse zurück.'''
